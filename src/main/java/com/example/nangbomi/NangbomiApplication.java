5e172191f1a7cbb5eb7a6c2d8629da9089fed1b1
KKT <kimgt2828@gmail.com>
2024-08-21 17:52:42 +0900

    setting: init settings - 의존성 추가

---
+++ src/main/java/com/example/nangbomi/NangbomiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nangbomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NangbomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NangbomiApplication.class, args);
+	}
+
+}
